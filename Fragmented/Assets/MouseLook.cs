f6a763bfe48d2ba792038501615b1083d6542ee5
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-01-18 15:01:01 +0000

    Looking done

--- Fragmented/Assets/MouseLook.cs
+++ Fragmented/Assets/MouseLook.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 500f;
     
     public Transform playerBody;
+
+    float xRotation = 0f;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -21,6 +23,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis ("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis ("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0F, 0F);
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
