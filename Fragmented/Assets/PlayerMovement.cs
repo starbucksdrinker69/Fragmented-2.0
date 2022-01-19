240a9afb572bc20322e27359db4e8b88ca26f5f4
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-01-19 12:40:18 +0000

    pain

--- Fragmented/Assets/PlayerMovement.cs
+++ Fragmented/Assets/PlayerMovement.cs
@@ -38,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Debug.Log (5);
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
